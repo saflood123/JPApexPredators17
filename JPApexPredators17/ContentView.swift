066b2d549d3bec863d744a1574f6d4ea9a00dfba
saflood123 <stuartflood@gmail.com>
2024-11-03 09:58:16 +0000

    Initial Commit

---
+++ JPApexPredators17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JPApexPredators17
+//
+//  Created by stuart flood on 03/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
